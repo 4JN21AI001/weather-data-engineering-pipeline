fc81f63b3d5bc8886cdd50c628cb8d8035f50660
Abhishek Badiger <152054306+4JN21AI001@users.noreply.github.com>
2025-12-02 11:31:27 +0530

    Update 01_create_db_schema.sql

--- sql/01_create_db_schema.sql
+++ sql/01_create_db_schema.sql
@@ -1,1 +1,11 @@
 
+-- Create Warehouse access
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE compute_wh;
+
+-- Create Database & Schema
+CREATE OR REPLACE DATABASE weather_snow;
+USE DATABASE weather_snow;
+
+CREATE OR REPLACE SCHEMA curated;
+USE SCHEMA curated;
